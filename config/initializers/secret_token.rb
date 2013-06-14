bd36cdc5995ae15aa0ba92f51058dad21160184f
Dongkuk <dongkuk5411@nhnnext.org>
2013-06-14 22:23:43 +0900

    Arbos 프로젝트 시작!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Arbos::Application.config.secret_key_base = '0e8f3f44796402f357fd4afc658587a9ac5ebea0c7e63ffc8bb1654a7c11c0d190b2c5ef05d3cce7eab8dcdfebe104831589627059ee7425c698eae35cf8d6a8'

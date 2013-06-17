41305a93ed7426fc61ee7d8439fde218ea7daa61
Dongkuk <dongkuk5411@nhnnext.org>
2013-06-17 21:05:56 +0900

    #8 배포 자동화

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,11 +21,26 @@ set :use_sudo, false
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+# Generate an additional task to fire up the thin clusters
+namespace :deploy do
+  desc "Start the Thin processes"
+  task :start do
+    run  <<-CMD
+      cd /u/apps/arbos/current; bundle exec thin start -C config/thin.yml
+    CMD
+  end
+
+  desc "Stop the Thin processes"
+  task :stop do
+    run <<-CMD
+      cd /u/apps/arbos/current; bundle exec thin stop -C config/thin.yml
+    CMD
+  end
+
+  desc "Restart the Thin processes"
+  task :restart do
+    run <<-CMD
+      cd /u/apps/arbos/current; bundle exec thin restart -C config/thin.yml
+    CMD
+  end
+end
